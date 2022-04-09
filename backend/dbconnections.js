7b0f1ca499fade1f5eaddf3100dff31493efe20c
Drael Davidson <drael_davidson@hotmail.com>
2022-04-08 23:05:14 -0700

    backend stuff

---
+++ backend/dbconnections.js
@@ -0,0 +1,13 @@
+//this is connects to my tables in postgres
+//I MADE A NEW DATABASE TITLED project one to put the tables into
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user:'postgres',
+    host:'localhost',
+    database:'projectone',
+    password:'password',
+    port:5432
+});
+
+module.exports=pool;
