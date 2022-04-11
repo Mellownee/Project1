3d8a36c812bcc5c95dcca52655d0677a1c3fedeb
= <=>
2022-04-11 12:28:24 -0700

    commit on 'main'

---
+++ frontend/src/components/server.js
@@ -0,0 +1,38 @@
+// const express = require ('express');
+// const app = express ();
+// const { users } = require('.data');
+// const reimbursementRouter = require('./routes/reimbursement');
+
+app.use(express.json());
+app.use(setUser);
+app.use('/projects', requestRouter);
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+})
+
+app.get('/dashboard', (req, res) => {
+    res.send('Dashboard Page');
+})
+
+app.get('/supervisor', (req, res) => {
+    res.send('Supervisor Page');
+})
+
+app.get('/manager', (req, res) => {
+    res.send('Manager Page');
+})
+
+app.get('/admin', (req, res) => {
+    res.send('Admin Page');
+})
+
+function setUser(req, res, next) {
+    const userId = req.body.userId
+    if (userId) {
+        req.user = users.find(user => user.id === userId);
+    }
+    next();
+}
+
+app.listen(3000);
