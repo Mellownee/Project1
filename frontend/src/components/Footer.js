2b9e7e301be59de676ac0dc7af544f7f96677293
Fatima Baloch <fatima_baluch@hotmail.com>
2022-04-17 14:03:28 -0700

    added social media at the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,36 +8,39 @@ function Footer() {
         <div className="row">
           {/* Column1 */}
           <div className="col">
-            <h4>THICC MEMES INC</h4>
+            <h4> Audacity Reimbursment Program</h4>
             <h1 className="list-unstyled">
-              <li>342-420-6969</li>
+            
               <li>Seattle, USA</li>
               <li>123 Streeet South North</li>
             </h1>
           </div>
           {/* Column2 */}
+       
+
           <div className="col">
-            <h4>Stuff</h4>
+            <h4>Contact us</h4>
             <ui className="list-unstyled">
-              <li>DANK MEMES</li>
-              <li>OTHER STUFF</li>
-              <li>GUD STUFF</li>
+            <li>342-420-6969</li>
+            </ui>
+          </div>
+
+          <div className="col">
+            <h4>feedback</h4>
+            <ui className="list-unstyled">
+              <li><a href="https://www.facebook.com/" class="fa fa-facebook"></a></li>
+              <li><a href="https://www.instagram.com/?hl=en" class="fa fa-instagram"></a></li>
+              <li><a href="#" class="fa fa-twitter"></a></li>
             </ui>
           </div>
           {/* Column3 */}
-          <div className="col">
-            <h4>WELL ANOTHER COLUMN</h4>
-            <ui className="list-unstyled">
-              <li>DANK MEMES</li>
-              <li>OTHER STUFF</li>
-              <li>GUD STUFF</li>
-            </ui>
-          </div>
         </div>
+        
+        
         <hr />
         <div className="row">
           <p className="col-sm">
-            &copy;{new Date().getFullYear()} THICC MEMES | All rights reserved |
+            &copy;{new Date().getFullYear()}  Audacity Reimbursment Program | All rights reserved |
             Terms Of Service | Privacy
           </p>
         </div>
